9673a49a0cce8df34fa37f25103699ff44a80c02
Rohit Joshi <rohitjoshi3214@gmail.com>
2024-09-20 13:59:15 +0530

    commit

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,26 +1,11 @@
 import React, { useEffect } from 'react'
 import logo from './../assets/logo.png'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { signOut } from 'firebase/auth'
 import { auth } from '../utils/firebase'
-import { api_option } from '../utils/constants'
-import { useDispatch } from 'react-redux'
-import { addNowPlayingMovies } from '../utils/movieSlice'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 
 export const Entry = () => {
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', api_option)
-    const json = await data.json();
-    console.log(json.results)
-    dispatch(addNowPlayingMovies(json.results))
-  }
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, [])
-  const navigate = useNavigate()
   const handleSignOut = () => {
     signOut(auth).then(() => {
       navigate('/login')
